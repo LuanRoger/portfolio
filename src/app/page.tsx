2815bf02574e65cdf96c82c99e3f5142cb1f2302
ROG <luan.roger.2003@gmail.com>
2024-09-21 17:40:46 -0300

    feat: incorporate svg into React components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import NameHeader from "@/components/name-header";
 import { NoiseContainer } from "@/components/grayni-noise";
+import { DartIcon } from "@/components/svg-icons";
 
 export default function Home() {
   return (
     <main className="relative rounded-md mx-56 mt-24 p-4">
       <div className="z-30">
         <NameHeader />
+        <DartIcon />
       </div>
       <NoiseContainer className="rounded-md" />
     </main>
