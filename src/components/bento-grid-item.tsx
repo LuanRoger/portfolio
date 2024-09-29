927af205c35eec5509583ba183edc531d1038703
LuanRoger <luan.roger.2003@gmail.com>
2024-09-29 17:45:34 -0300

    feat: add project grid

---
+++ src/components/bento-grid-item.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { cn } from "@/lib/shadcn-utils";
+import { AnimatePresence, motion } from "framer-motion";
+
+interface BentoGridItemProps {
+  className?: string;
+  title?: string | React.ReactNode;
+  support?: string | React.ReactNode;
+  links?: (string | React.ReactNode)[];
+  description?: string | React.ReactNode;
+  header?: React.ReactNode;
+  banner?: React.ReactNode;
+}
+
+export default function BentoGridItem({
+  className,
+  title,
+  support,
+  links,
+  description,
+  header,
+  banner,
+}: BentoGridItemProps) {
+  const supportVariants = { hover: { opacity: 1 } };
+  const descriptionVariants = {
+    hover: { bottom: -80 },
+  };
+  const descriptionContainerVariants = {
+    hover: { opacity: 0.8 },
+  };
+  const linksContainerVariants = {
+    hover: { backgroundColor: "#09090B", transition: { delay: 0.2 } },
+  };
+  const linksVariants = { hover: { y: 0, opacity: 1 } };
+
+  return (
+    <motion.div
+      whileHover={"hover"}
+      whileTap={"hover"}
+      className={cn(
+        "relative row-span-1 rounded-xl overflow-clip border border-border justify-between flex flex-col",
+        className
+      )}
+    >
+      <motion.div
+        initial={{ opacity: 0.5 }}
+        variants={supportVariants}
+        className="transition-all absolute flex flex-row flex-wrap gap-2 left-0 top-0
+            z-30 bg-gradient-to-b from-background w-full p-2"
+      >
+        {support}
+      </motion.div>
+      <motion.div
+        variants={descriptionVariants}
+        className="flex flex-col items-end gap-2 z-30 max-h-full w-full absolute left-0 bottom-0"
+      >
+        <motion.div
+          variants={linksContainerVariants}
+          className="mx-2 inline-flex gap-2 rounded-md p-1"
+        >
+          {links && (
+            <AnimatePresence>
+              {links.map((link, index) => (
+                <motion.div
+                  key={index}
+                  initial={{ y: 10, opacity: 0 }}
+                  variants={linksVariants}
+                >
+                  {link}
+                </motion.div>
+              ))}
+            </AnimatePresence>
+          )}
+        </motion.div>
+        <motion.div
+          variants={descriptionContainerVariants}
+          className="w-full h-full fnoise space-y-2 p-4"
+        >
+          {header}
+          <div className="font-bold">{title}</div>
+          <div className="font-normal text-sm">{description}</div>
+        </motion.div>
+      </motion.div>
+      <div className="transition absolute z-10 w-full h-full">{banner}</div>
+    </motion.div>
+  );
+}
---
+++ src/components/bento-grid-project-item.tsx
@@ -0,0 +1,62 @@
+import { Project } from "@/lib/types/project";
+import Image from "next/image";
+import { GitHubIcon } from "./svg-icons";
+import MinimalTechBadge from "./minimal-tech-badge";
+import BentoGridItem from "./bento-grid-item";
+import { IconExternalLink } from "@tabler/icons-react";
+import { Button } from "./ui/button";
+
+interface BentoGridProjectItemProps {
+  project: Project;
+  className?: string | undefined;
+}
+
+export default function BentoGridProjectItem({
+  project,
+  className,
+}: BentoGridProjectItemProps) {
+  const { title, imageSrc, imageAlt, description, link, repoLink, techStack } =
+    project;
+
+  return (
+    <BentoGridItem
+      className={className}
+      title={title}
+      support={
+        <>
+          {techStack.map((tech) => (
+            <MinimalTechBadge key={tech.name} tech={tech} />
+          ))}
+        </>
+      }
+      links={[
+        repoLink && (
+          <Button variant="ghost" size="icon">
+            <a key={"github"} href={repoLink} target="_blank">
+              <GitHubIcon className="w-5 h-5 fill-foreground" />
+            </a>
+          </Button>
+        ),
+        link && (
+          <Button variant="ghost" size="icon">
+            <a key={"link"} href={link} target="_blank">
+              <IconExternalLink className="w-5 h-5 fill-foreground" />
+            </a>
+          </Button>
+        ),
+      ]}
+      description={
+        <div className="flex flex-col gap-4">
+          <p>{description}</p>
+        </div>
+      }
+      banner={
+        <Image
+          src={imageSrc}
+          alt={imageAlt}
+          className="w-full h-full object-cover"
+        />
+      }
+    />
+  );
+}
