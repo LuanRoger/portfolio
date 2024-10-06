b34e2fe5f0e9b9f930fac534cf48b9ece12305b6
LuanRoger <luan.roger.2003@gmail.com>
2024-10-06 19:03:49 -0300

    feat: create badge marquee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,16 @@ import NavigationHeader from "@/components/sections/navigation-header";
 import ProjectsSection from "@/components/sections/projects-section";
 import ExpirienceSection from "@/components/sections/expirience-section";
 import ContactSection from "@/components/sections/contact-section";
+import FooterSection from "@/components/sections/footer-section";
 
 export default function Home() {
   return (
-    <div className="relative md:max-2xl:rounded-md p-4 
+    <div
+      className="relative md:max-2xl:rounded-md 
     md:max-2xl:border border-border md:max-2xl:w-3/5 z-20
-    overflow-clip">
-      <div className="space-y-2">
+    overflow-clip"
+    >
+      <div className="space-y-2 p-4">
         <NameHeader />
         <TechStack />
         <Separator />
@@ -23,6 +26,7 @@ export default function Home() {
         <ExpirienceSection />
         <ContactSection />
       </div>
+      <FooterSection />
       <NoiseContainer className="rounded-md" />
     </div>
   );
---
+++ src/components/sections/badge-marquee.tsx
@@ -0,0 +1,66 @@
+import Marquee from "react-fast-marquee";
+import Image from "next/image";
+import FinalFantasyBadge from "~/public/images/finalfantasy.gif";
+import BrasilFlagBadge from "~/public/images/brasil.gif";
+import ChronoBadge from "~/public/images/chrono.png";
+import ZeldaBadge from "~/public/images/zelda.png";
+import CssValid from "~/public/images/cssvalid.gif";
+import IloveMusic from "~/public/images/ilovemusic.png";
+import MozillaBadge from "~/public/images/mozilla.gif";
+import Mp3LoveBadge from "~/public/images/mp3love.gif";
+import NotepadBadge from "~/public/images/notepad.gif";
+
+export default function BadgeMarquee() {
+  return (
+    <div className="opacity-80">
+      <Marquee speed={20}>
+        <span className="inline-flex gap-2">
+          <Image
+            src={FinalFantasyBadge}
+            alt="Final Fantasy Badge"
+            width={80}
+            height={15}
+          />
+          <Image
+            src={BrasilFlagBadge}
+            alt="Brasil Badge"
+            width={80}
+            height={15}
+          />
+          <Image
+            src={ChronoBadge}
+            alt="Chrono Trigger Badge"
+            width={80}
+            height={15}
+          />
+          <Image src={ZeldaBadge} alt="Zelda Badge" width={80} height={15} />
+          <Image src={CssValid} alt="CSS Valid Badge" width={80} height={15} />
+          <Image
+            src={IloveMusic}
+            alt="I Love Music Badge"
+            width={80}
+            height={15}
+          />
+          <Image
+            src={MozillaBadge}
+            alt="Mozilla Badge"
+            width={80}
+            height={15}
+          />
+          <Image
+            src={Mp3LoveBadge}
+            alt="Mp3 Love Badge"
+            width={80}
+            height={15}
+          />
+          <Image
+            src={NotepadBadge}
+            alt="Notepad Badge"
+            width={80}
+            height={15}
+          />
+        </span>
+      </Marquee>
+    </div>
+  );
+}
