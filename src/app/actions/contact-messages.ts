0acc28d9541aa8ad03f27cc60f3925109d79f1e3
LuanRoger <luan.roger.2003@gmail.com>
2024-10-09 10:11:31 -0300

    feat: implement ratelimiter for form server action

--- src/app/actions/contact-messages.ts
+++ src/app/actions/contact-messages.ts
@@ -2,8 +2,14 @@
 
 import { registerMessage } from "@/lib/db/quries/contact-messages";
 import { contactFormSchema } from "@/lib/types/forms/contact-form-schema";
+import { checkRateLimitAction } from "./ratelimit";
 
 export async function sendMessageAction(formData: FormData) {
+  const rateLimitSuccess = await checkRateLimitAction();
+  if (!rateLimitSuccess) {
+    return null;
+  }
+
   const { data, success } = await contactFormSchema.safeParseAsync({
     name: formData.get("name"),
     email: formData.get("email"),
