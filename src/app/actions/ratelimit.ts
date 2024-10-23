9d9ea9a434faa2317dda350e4131c108841ef64a
LuanRoger <luan.roger.2003@gmail.com>
2024-10-23 09:42:38 -0300

    feat: update to Next 15

--- src/app/actions/ratelimit.ts
+++ src/app/actions/ratelimit.ts
@@ -6,7 +6,7 @@ import { ratelimit } from "@/lib/kv/ratelimit";
 export async function checkRateLimitAction() {
   const FALLBACK_IP_ADDRESS = "0.0.0.0";
 
-  const ip = headers().get("x-real-ip") ?? FALLBACK_IP_ADDRESS;
+  const ip = (await headers()).get("x-real-ip") ?? FALLBACK_IP_ADDRESS;
   console.log("Checking rate limit for IP", ip);
 
   const { success } = await ratelimit.limit(ip);
