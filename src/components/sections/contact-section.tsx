331a035a09390d0d8ea62a304a7f3814d2b220aa
LuanRoger <luan.roger.2003@gmail.com>
2024-11-03 11:58:35 -0300

    fix: remove quick form badge

--- src/components/sections/contact-section.tsx
+++ src/components/sections/contact-section.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import QuickContactForm from "../quick-contact-form";
-import QuickFormBadge from "~/public/images/quickform.gif";
 import Link from "../link";
 
 export default function ContactSection() {
@@ -15,13 +13,6 @@ export default function ContactSection() {
         , or send me a quick message using the form below:
       </p>
       <div className="p-4 border border-border rounded-md space-y-2">
-        <Image
-          src={QuickFormBadge}
-          alt="Quick Contact Form"
-          width={80}
-          height={15}
-          className="opacity-80"
-        />
         <QuickContactForm />
       </div>
     </section>
