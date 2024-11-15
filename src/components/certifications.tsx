26607f7c457550a589a82b0b46d5ce82d18d806b
LuanRoger <luan.roger.2003@gmail.com>
2024-11-15 08:56:17 -0300

    feat: add link to certification

--- src/components/certifications.tsx
+++ src/components/certifications.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
 import GitHubFoundations from "~/public/images/github-foundations.png";
+import Link from "./link";
 
 export default function Certifications() {
   return (
     <div className="flex flex-row justify-evenly">
-      <Image
-        src={GitHubFoundations}
-        width={150}
-        height={150}
-        alt="GitHub Foundations Certificate Badge"
-      />
+      <Link target="_blank" href="https://www.credly.com/badges/765dfe92-ebdf-4c5d-8839-75b674a8360f/linked_in_profile">
+        <Image
+          src={GitHubFoundations}
+          width={150}
+          height={150}
+          alt="GitHub Foundations Certificate Badge"
+        />
+      </Link>
     </div>
   );
 }
