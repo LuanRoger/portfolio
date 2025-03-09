80e869d430366cfb3b013bffb5295afc2305daca
ROG <luan.roger.2003@gmail.com>
2025-03-09 09:32:51 -0300

    feat: remove rate limit

--- src/lib/kv/ratelimit.ts
+++
@@ -1,8 +0,0 @@
-import { Ratelimit } from "@upstash/ratelimit";
-import { kv } from "@vercel/kv";
-
-export const ratelimit = new Ratelimit({
-  redis: kv,
-  // 5 requests from the same IP in 10 seconds
-  limiter: Ratelimit.fixedWindow(1, "60 s"),
-});
