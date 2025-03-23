3084ffcff3f6082e0ca98745849096252ef9bb98
ROG <luan.roger.2003@gmail.com>
2025-03-23 14:25:45 -0300

    feat: implement all time metrics

--- src/components/coding-status-collapser.tsx
+++ src/components/coding-status-collapser.tsx
@@ -8,6 +8,8 @@ import { IconChartBar } from "@tabler/icons-react";
 import { WakaTimeIcon } from "./svg-icons";
 import WakatimeLanguages from "./charts/wakatime-language";
 import WakatimeSummaryCategories from "./charts/wakatime-summary";
+import Link from "./link";
+import WakatimeAllTime from "./charts/wakatime-alltime";
 
 export default async function CodingStatusCollapser() {
   return (
@@ -22,12 +24,17 @@ export default async function CodingStatusCollapser() {
         <AccordionContent className="flex flex-col gap-4">
           <WakatimeLanguages />
           <WakatimeSummaryCategories />
+          <WakatimeAllTime />
           <div className="flex justify-between text-sm text-muted-foreground">
-            <p>Collecting data since 22 December 2022</p>
-            <span className="inline-block gap-2">
+            <p>Collecting data since 23 December 2022</p>
+            <Link
+              href={"https://wakatime.com"}
+              isExternal
+              className="inline-block gap-2 font-medium"
+            >
               Powered by{" "}
               <WakaTimeIcon className="inline-block fill-muted-foreground size-5" />
-            </span>
+            </Link>
           </div>
         </AccordionContent>
       </AccordionItem>
