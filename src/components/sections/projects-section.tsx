40b1c2e98d86fcc42d125539cd600173253dc0d7
ROG <luan.roger.2003@gmail.com>
2025-03-23 14:57:06 -0300

    feat: add link to ready to use projects

--- src/components/sections/projects-section.tsx
+++ src/components/sections/projects-section.tsx
@@ -1,3 +1,4 @@
+import Link from "../link";
 import MyProjectsGrid from "../my-projects-grid";
 
 export default function ProjectsSection() {
@@ -5,6 +6,12 @@ export default function ProjectsSection() {
     <section id="projects" className="space-y-2">
       <h2 className="text-xl font-bold">Projects</h2>
       <MyProjectsGrid />
+      <Link
+        href={"https://github.com/stars/LuanRoger/lists/ready-to-use"}
+        className="text-sm"
+      >
+        Check out more 🚀
+      </Link>
     </section>
   );
 }
