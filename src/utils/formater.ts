6318d6063adb68551416c716e997632e1cc0976b
ROG <luan.roger.2003@gmail.com>
2025-07-13 13:31:58 -0300

    feat: add github languages colors

--- src/utils/formater.ts
+++ src/utils/formater.ts
@@ -1,3 +1,8 @@
 export function formatSpotifyArtistsName(artists: string[]) {
   return artists.map((artist) => artist).join(", ");
 }
+
+export function formatBytesInMB(bytes: number) {
+  const megabytes = bytes / (1024 * 1024);
+  return `${megabytes.toFixed(2)} MB`;
+}
