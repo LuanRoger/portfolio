f0b5d79afb01d11c4c63ea744bd28bd43c3d0d9c
ROG <luan.roger.2003@gmail.com>
2025-07-13 13:32:32 -0300

    refactor: move the accordion item to own component

---
+++ src/components/wakatime-coding-metrics-accordion-item.tsx
@@ -0,0 +1,53 @@
+import { IconChartBar } from "@tabler/icons-react";
+import { WakaTimeIcon } from "./svg-icons";
+import WakatimeLanguages from "./charts/wakatime-language";
+import WakatimeSummaryCategories from "./charts/wakatime-summary";
+import Link from "./link";
+import WakatimeRawMetrics from "./charts/wakatime-raw-metrics";
+import { Suspense } from "react";
+import {
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./ui/accordion";
+
+interface WakatimeCodingMetricsAccordionItemProps {
+  value: string;
+}
+
+export default function WakatimeCodingMetricsAccordionItem({
+  value,
+}: WakatimeCodingMetricsAccordionItemProps) {
+  return (
+    <AccordionItem value={value}>
+      <AccordionTrigger>
+        <span className="inline-flex items-center gap-2">
+          <IconChartBar size={16} />
+          Metrics
+        </span>
+      </AccordionTrigger>
+      <AccordionContent className="flex flex-col gap-4">
+        <Suspense>
+          <WakatimeLanguages />
+          <WakatimeSummaryCategories />
+          <WakatimeRawMetrics />
+        </Suspense>
+        <div className="text-muted-foreground flex justify-between text-sm">
+          <p>
+            Collecting data since 23 December 2022.
+            <br />
+            Some informations are not update with today&apos;s data.
+          </p>
+          <Link
+            href={"https://wakatime.com"}
+            isExternal
+            className="gap-2 self-end font-medium"
+          >
+            Powered by{" "}
+            <WakaTimeIcon className="fill-muted-foreground inline-block size-5" />
+          </Link>
+        </div>
+      </AccordionContent>
+    </AccordionItem>
+  );
+}
