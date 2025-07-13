e7894b504676e9ff660a084c19482ea840fa33d5
ROG <luan.roger.2003@gmail.com>
2025-07-13 14:25:29 -0300

    feat: create component to show the user info

--- src/components/github-profile-metrics.tsx
+++ src/components/github-profile-metrics.tsx
@@ -1,29 +1,11 @@
-import { getGithubProfile } from "@/app/actions/github";
 import GitHubLanguages from "./charts/github-languages";
+import GitHubProfileInfo from "./github-profile-info";
 
 export default async function GithubProfileMetrics() {
-  const userData = await getGithubProfile();
-
-  if (!userData) {
-    return <p className="text-red-500">Failed to load GitHub profile.</p>;
-  }
-
   return (
     <div className="flex flex-col gap-4">
-      <p>Username: {userData.login}</p>
-      <p>Public Repos: {userData.publicRepos}</p>
-      <p>Followers: {userData.followers}</p>
-      <a
-        href={userData.htmlUrl}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-blue-500 hover:underline"
-      >
-        View Profile on GitHub
-      </a>
-      <div>
-        <GitHubLanguages />
-      </div>
+      <GitHubProfileInfo />
+      <GitHubLanguages />
     </div>
   );
 }
