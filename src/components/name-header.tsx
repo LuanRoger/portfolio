68983ffa456c8acebaa09ddb53d17aea68d4420a
ROG <luan.roger.2003@gmail.com>
2026-01-18 10:45:50 -0300

    feat: simple H1 for name

--- src/components/name-header.tsx
+++ src/components/name-header.tsx
@@ -1,14 +1,15 @@
 import ActivitiesDialog, {
   ActivitiesDialogLoading,
 } from "@/components/activities-dialog";
-import Name from "./name";
 import { Suspense } from "react";
 
 export default function NameHeader() {
   return (
     <header>
       <div className="flex flex-row justify-between">
-        <Name />
+        <h1 className="cursor-default text-4xl font-bold transition-transform hover:scale-105">
+          Luan Roger
+        </h1>
         <Suspense fallback={<ActivitiesDialogLoading />}>
           <ActivitiesDialog />
         </Suspense>
