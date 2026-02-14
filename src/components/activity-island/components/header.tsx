a4351c868d4de9f6ecb8d0363920155ff698972b
ROG <luan.roger.2003@gmail.com>
2026-02-14 15:00:23 -0300

    feat: create offline variant

--- src/components/activity-island/components/header.tsx
+++ src/components/activity-island/components/header.tsx
@@ -1,10 +1,33 @@
 import Dot from "@/components/dot";
+import type { Presence } from "@/types/presence";
+import { cn } from "@/utils/shadcn-utils";
 
-export default function ActivityDialogHeader() {
+type ActivityDialogHeaderProps = {
+  status: Presence;
+};
+
+export default function ActivityDialogHeader({
+  status,
+}: ActivityDialogHeaderProps) {
   return (
-    <div className="inset-shadow-green-700 inset-shadow-xs flex w-full items-center justify-center gap-1 rounded-t-md bg-green-500 p-1 font-semibold text-sm text-white">
-      <Dot animate />
-      online
+    <div
+      className={cn(
+        "inset-shadow-xs flex w-full items-center justify-center gap-1 rounded-t-md px-2 py-1 font-semibold text-sm text-white",
+        {
+          "inset-shadow-green-700 bg-green-500": status === "online",
+          "inset-shadow-gray-700 rounded-b-md bg-gray-500":
+            status === "offline",
+        }
+      )}
+    >
+      <Dot
+        animate={status === "online"}
+        dotClassName={cn({
+          "bg-green-400": status === "online",
+          "bg-gray-400": status === "offline",
+        })}
+      />
+      {status.toString()}
     </div>
   );
 }
