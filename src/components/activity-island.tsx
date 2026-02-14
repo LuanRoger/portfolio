c8b3e28331b403756d3ac76041de8e9c640bcd9c
ROG <luan.roger.2003@gmail.com>
2026-02-14 11:37:46 -0300

    feat: revamped UI in activity island

--- src/components/activity-island.tsx
+++ src/components/activity-island.tsx
@@ -1,7 +1,6 @@
 import type { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 import { cn } from "@/utils/shadcn-utils";
-import { FadeVignette } from "./fade-vignette";
 import Link from "./link";
 
 type ActivityIslandProps = {
@@ -27,46 +26,54 @@ export default function ActivityIsland({
   href,
   accentColor,
 }: ActivityIslandProps) {
-  const titleDefaultClasses = "text-white font-bold";
+  const titleDefaultClasses =
+    "text-white font-bold text-sm text-ellipsis line-clamp-2";
 
   return (
-    <div className="relative inset-shadow-xs isolate h-40 w-full overflow-clip rounded-lg">
-      {accentColor && <FadeVignette accentColor={accentColor} />}
+    <div className="relative h-28 w-full overflow-clip">
+      <div
+        className="absolute inset-ring inset-ring-primary inset-shadow-sm z-10 size-full"
+        style={
+          accentColor
+            ? { boxShadow: `inset 0 0 10px ${accentColor}` }
+            : undefined
+        }
+      />
       <Image
         alt={imageAlt}
-        className="absolute inset-0 z-0 size-full object-none"
+        className="absolute inset-0 z-10 size-full object-none opacity-50"
         height={300}
         src={imageSrc}
         width={300}
       />
       <Image
         alt={imageAlt}
-        className="absolute inset-0 -z-10 size-full object-cover blur-lg"
+        className="absolute inset-0 -z-10 size-full object-cover opacity-50 blur-lg"
         height={30}
         src={imageSrc}
         width={30}
       />
-      <div className="absolute z-20 flex size-full flex-col justify-between p-2">
-        <div className="inline-flex items-center gap-2">
+      <div className="absolute z-10 flex size-full flex-col justify-between p-2">
+        <div className="inline-flex gap-1 text-xs">
           <Image
             alt={iconAlt}
             className="invert filter"
-            height={18}
+            height={12}
             src={iconSrc}
-            width={18}
+            width={12}
           />
-          <h2 className="text-sm">{activityDescription}</h2>
+          {activityDescription}
         </div>
-        <div>
+        <p>
           {href ? (
             <Link className={cn(titleDefaultClasses)} href={href} isExternal>
               {title}
             </Link>
           ) : (
-            <h1 className={cn(titleDefaultClasses)}>{title}</h1>
+            <span className={cn(titleDefaultClasses)}>{title}</span>
           )}
-          <h2 className="text-sm text-white">{description}</h2>
-        </div>
+          <span className="text-sm text-white">{description}</span>
+        </p>
       </div>
     </div>
   );
