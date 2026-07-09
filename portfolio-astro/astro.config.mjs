1b23666a5bc1c3a9a621cb3b89e4c639abc4e314
ROG <luanroger.dev@gmail.com>
2026-07-09 10:21:15 -0300

    feat: setup varlock

--- portfolio-astro/astro.config.mjs
+++ portfolio-astro/astro.config.mjs
@@ -1,15 +1,20 @@
 // @ts-check
 import { defineConfig, svgoOptimizer } from 'astro/config';
+import varlockAstroIntegration from '@varlock/astro-integration';
 
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
-  integrations: [react()],
+  output: 'server',
+  adapter: vercel(),
+  integrations: [varlockAstroIntegration(), react()],
   vite: {
     plugins: [tailwindcss()]
   },
   experimental: {
     svgOptimizer: svgoOptimizer()
-  }
+  },
 });
