121b76167e13aeefd318648b03a2a1a16c5dbe36
ROG <luanroger.dev@gmail.com>
2026-07-19 11:27:24 -0300

    fix: specify user agent

--- portfolio-astro/src/actions/github/index.ts
+++ portfolio-astro/src/actions/github/index.ts
@@ -8,6 +8,7 @@ import {
 } from "./adapters";
 import type { GitHubRepository } from "@/types/github";
 import { ActionError, defineAction } from "astro:actions";
+import { GITHUB_USER_AGENTS } from "@/lib/constants/github";
 
 async function getGithubRepositoryLanguages(languageUrl: string) {
   const githubToken = ENV.GITHUB_TOKEN;
@@ -15,20 +16,21 @@ async function getGithubRepositoryLanguages(languageUrl: string) {
     throw new ActionError({
       code: "INTERNAL_SERVER_ERROR",
       message: "GitHub token is required",
-    })
+    });
   }
 
   const result = await fetch(languageUrl, {
     headers: {
       Authorization: `Bearer ${githubToken}`,
       Accept: "application/vnd.github+json",
+      "User-Agent": GITHUB_USER_AGENTS,
     },
   });
 
   const jsonResult = await result.json();
   const languages =
     adaptGitHubRepositoryLanguagesResponseToGitHubRepositoryLanguages(
-      jsonResult
+      jsonResult,
     );
 
   return languages;
@@ -42,13 +44,14 @@ const getGithubProfile = defineAction({
       throw new ActionError({
         code: "INTERNAL_SERVER_ERROR",
         message: "GitHub token and API URL are required",
-      })
+      });
     }
 
     const result = await fetch(`${gitHubApiUrl}/user`, {
       headers: {
         Authorization: `Bearer ${githubToken}`,
         Accept: "application/vnd.github+json",
+        "User-Agent": GITHUB_USER_AGENTS,
       },
     });
 
@@ -56,13 +59,13 @@ const getGithubProfile = defineAction({
       throw new ActionError({
         code: "INTERNAL_SERVER_ERROR",
         message: "Failed to fetch GitHub profile",
-      })
+      });
     }
 
     const jsonResult = await result.json();
     return adaptGitHubUserResponseToGitHubUser(jsonResult);
-  }
-})
+  },
+});
 
 const getGithubProfileRepositories = defineAction({
   handler: async () => {
@@ -72,7 +75,7 @@ const getGithubProfileRepositories = defineAction({
       throw new ActionError({
         code: "INTERNAL_SERVER_ERROR",
         message: "GitHub token is required",
-      })
+      });
     }
 
     const result = await fetch(
@@ -81,23 +84,24 @@ const getGithubProfileRepositories = defineAction({
         headers: {
           Authorization: `Bearer ${githubToken}`,
           Accept: "application/vnd.github+json",
+          "User-Agent": GITHUB_USER_AGENTS,
         },
-      }
+      },
     );
 
     if (!result.ok) {
       throw new ActionError({
         code: "INTERNAL_SERVER_ERROR",
         message: "Failed to fetch GitHub repositories",
-      })
+      });
     }
 
-    const jsonResult = await result.json();
+    const jsonResult: unknown[] = await result.json();
     const repositories: GitHubRepository[] = jsonResult.map(
-      adaptGitHubRepositoryResponseToGitHubRepository
+      adaptGitHubRepositoryResponseToGitHubRepository,
     );
     const filteredRepositories = repositories.filter(
-      (repo) => !repo.fork && repo.private === false
+      (repo) => !repo.fork && repo.private === false,
     );
 
     const languagesResults = await Promise.all(
@@ -107,7 +111,7 @@ const getGithubProfileRepositories = defineAction({
           id: repo.id,
           languages: languages || [],
         };
-      })
+      }),
     );
 
     for (const result of languagesResults) {
@@ -118,10 +122,10 @@ const getGithubProfileRepositories = defineAction({
     }
 
     return filteredRepositories;
-  }
-})
+  },
+});
 
 export const github = {
   getGithubProfile,
   getGithubProfileRepositories,
-}
+};
